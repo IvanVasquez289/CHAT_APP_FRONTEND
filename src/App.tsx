71ab69e29606e35267c510f43012d9096db0987d
ivan <ivanjv1234@gmail.com>
2025-06-12 13:29:44 -0600

    feat: create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-
+import Navbar from "./components/Navbar"
+import {Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
 const App = () => {
   return (
     <div>
-      <p className="text-red-500">Hola mundo</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
